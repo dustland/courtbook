bc3131765294a3c2a596c95c3619c1c8a3c2ffdc
Hugh Lyu <hugh.lv@gmail.com>
2025-04-21 21:20:43 +0800

    fix: make the background of pulldown zone consistent with navigation bar

--- src/pages/index/index.config.ts
+++ src/pages/index/index.config.ts
@@ -2,5 +2,7 @@ export default definePageConfig({
   navigationBarTitleText: "CourtBook",
   navigationBarBackgroundColor: "#67b42e",
   navigationBarTextStyle: "white",
+  backgroundColor: "#67b42e",
+  backgroundTextStyle: "light",
   usingComponents: {},
 });
